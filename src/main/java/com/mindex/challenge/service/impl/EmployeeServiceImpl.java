2f375ff148528930fb1da537d86063442ca9c4c4
Nicholas Rudolph <rudolphn@usf.edu>
2022-06-04 21:52:46 -0400

    Task 1 & Task 2 completion

--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -29,7 +31,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee read(String id) {
-        LOG.debug("Creating employee with id [{}]", id);
+        LOG.debug("Reading employee with id [{}]", id);
 
         Employee employee = employeeRepository.findByEmployeeId(id);
 
@@ -46,4 +48,25 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return employeeRepository.save(employee);
     }
+
+    /** Computes the total number of reports under a given employee */
+    public int getTotalNumberOfReports(String id) {
+        int totalNumberOfReports = 0;
+
+        // Ideally this wouldn't happen, hopefully source doesn't give us nulls in List<>
+        Employee employee = this.read(id);
+        if (employee == null) {
+            throw new RuntimeException("Null employee!");
+        }
+
+        List<Employee> reports = employee.getDirectReports();
+        if (reports != null) {
+            for (Employee reportingEmployee : reports) {
+                totalNumberOfReports += 1 + getTotalNumberOfReports(reportingEmployee.getEmployeeId());
+            }
+        }
+
+        return totalNumberOfReports;
+    }
+
 }
